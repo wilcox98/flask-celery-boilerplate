5e544d6427456e1a263854a6b36905453e413d86
wilcox <rongunowilcox@gmail.com>
2023-12-14 17:54:15 +1100

    add sql support

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from celery import Celery, Task
 import celeryconfig
-
+from app.exts.extensions import db
 import os
 
 
@@ -34,6 +34,9 @@ def make_celery(app) -> Celery:
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    # configure the SQLite database, relative to the app instance folder
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    db.init_app(app)
     app.config.from_object("config")
     app.config.from_prefixed_env()
     make_celery(app)
@@ -42,4 +45,6 @@ def create_app() -> Flask:
     def view():
         return "Hello, Flask is up and running!"
 
+    with app.app_context():
+        db.create_all()
     return app
